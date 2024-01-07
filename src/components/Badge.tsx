9b91807d6ff633dcb7d235d129f3ca2812bce687
rcraw <amadeousonline@gmail.com>
2024-01-06 21:24:00 -0500

    rebranded2

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -60,7 +60,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Human Race</span>
+        <span> HumanRace.AI</span>
       </a>
     </span>
   );
